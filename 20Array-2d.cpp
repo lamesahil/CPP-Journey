ea0708a32d6b66da3c7981bdd5e8ae97c9088512
lamesahil <iamsahilt20@gmail.com>
2025-12-03 22:47:09 +0530

    update 20array

--- 20Array-2d.cpp
+++ 20Array-2d.cpp
@@ -1,15 +1,48 @@
 #include<iostream>
 using namespace std;
 //2D Array Example in C++
+
+//Spiral Matrix
+void printSpiral(int arr[][4], int n, int m) {
+    int top = 0, bottom = n - 1;
+    int left = 0, right = m - 1;
+
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++) {
+            cout << arr[top][i] << " ";
+        }
+        top++;
+        for (int i = top; i <= bottom; i++) {
+            cout << arr[i][right] << " ";
+        }
+        right--;
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--) {
+                cout << arr[bottom][i] << " ";
+            }
+            bottom--;
+        }
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                cout << arr[i][left] << " ";
+            }
+            left++;
+        }
+    }
+}
+
+
 int main() {
     // Declare a 2D array
-    int arr[3][4] = {
+    int arr[4][4] = {
         {1, 2, 3, 4},
         {5, 6, 7, 8},
-        {9, 10, 11, 12}
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
     };
 
     // Print the 2D array
+    /*
     cout << "2D Array elements:" << endl;
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 4; j++) {
@@ -33,7 +66,9 @@ int main() {
             cout << arr2[i][j] << " ";
         }
         cout << endl;
-    }
+    }*/
+    cout << "Spiral order of the 2D array:" << endl;
+    printSpiral(arr, 4, 4);
 
     return 0;
 }
