8a38fc8085baf55bd6b58880ae85c758ed8904e0
lamesahil <iamsahilt20@gmail.com>
2025-12-13 13:35:30 +0530

    update.

--- 23Vectors.cpp
+++ 23Vectors.cpp
@@ -1,5 +1,30 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+vector<int> pairSUM(vector<int>& arr, int target){
+    int st=0, end=arr.size()-1;
+    int currSUM=0;
+    vector<int> ans; //pair of indices
+    while(st<end){
+        currSUM = arr[st] + arr[end];
+        if(currSUM == target){
+            ans.push_back(st);
+            ans.push_back(end);
+            return ans;
+        }
+        else if(currSUM < target){
+            st++;
+        }
+        else{
+            end--;
+        }
+    }
+    return {-1, -1}; //pair not found
+    
+    
+}
+
 int main(){
     //Dynamic Arrary
     /* Dynamic arrays are allocated on the heap memory at runtime using pointers.
@@ -68,6 +93,71 @@ int main(){
 
    // while(true) { int* i = new int; } // memory leak example
 
+
+
+   //VECTORS
+   /* Vectors are dynamic arrays provided by the C++ Standard Template Library (STL).
+      They can resize themselves automatically when elements are added or removed.
+      Vectors provide various built-in functions for easy manipulation, such as push_back, pop_back, size, etc.
+    */
+    vector<int> vec; // Declaration of vector
+    vector<int> vec2 = {1, 2, 3, 4, 5}; // Initialization with values
+    vector<int> vec3(5, 10); // Vector of size 5, initialized with value 10
+
+    cout<< "Initial size of vec: " << vec.size() << endl;
+    cout<< "Initial size of vec2: " << vec2.size() << endl;
+    cout<< "Initial size of vec3: " << vec3.size() << endl;
+
+    //Output elements of vec2
+    for(int i=0; i<vec2.size(); i++){
+        cout << vec2[i] << " ";
+    }
+
+    //Vector IMplemantation in memory
+    vec2.push_back(20); // Adding element to the end
+    cout<< "Size of vec2 after push_back: " << vec2.size() << endl; //6
+    cout<<"Capacity of vec2: " << vec2.capacity() << endl;//10
+    vec2.pop_back(); // Removing last element
+    cout<< "Size of vec2 after pop_back: " << vec2.size() << endl;//5
+    cout<<"Capacity of vec2: " << vec2.capacity() << endl;//10 capacity doesn't decrease after pop_back
+
+    //2D Vector
+    // vector<vector<int>> vec2D; // Declaration of 2D vector
+    // vector<int> row1 = {1, 2, 3};   
+    // vector<int> row2 = {4, 5, 6};
+    // vec2D.push_back(row1);
+    // vec2D.push_back(row2);
+    // cout << "Elements of 2D vector:" << endl;
+    // for(int i=0; i<vec2D.size(); i++){
+    //     for(int j=0; j<vec2D[i].size(); j++){
+    //         cout << vec2D[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    vector<vector<int>> mat = {
+        {1, 2, 3},
+        {4, 5},
+        {6}
+    };
+    cout << "Elements of 2D vector (matrix):" << endl;
+    for(int i=0; i<mat.size(); i++){
+        for(int j=0; j<mat[i].size(); j++){
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    //Predict the output
+    vector<int> v;
+    for(int i=0; i<5; i++){
+        v.push_back(i); // Adding elements 0 to 4
+        cout << "Size after inserting " << i << ": " << v.size() << endl;
+        cout << "Capacity after inserting " << i << ": " << v.capacity() << endl;
+    }
+
+    vector<int> v1 = {1, 2, 3};
+cout << v1[10];
 
     return 0;     
 
