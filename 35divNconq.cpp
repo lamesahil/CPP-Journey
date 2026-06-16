6435ed03458035aacab27997bd976b16c9f76687
lamesahil <iamsahilt20@gmail.com>
2026-06-16 23:43:54 +0530

    update

---
+++ 35divNconq.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+void merge(int arr[], int si, int mid, int ei){
+    vector<int> temp; //temporary array
+    int i=si; //pointer for left half
+    int j=mid+1; //pointer for right half
+    //merge the two halves into temp
+    while(i<=mid && j<=ei){
+        if(arr[i]<arr[j]){
+            temp.push_back(arr[i++]);
+        }
+        else{
+            temp.push_back(arr[j++]);
+        }
+    }
+    while(i<=mid){
+        temp.push_back(arr[i++]);
+    }
+    while(j<=ei){
+        temp.push_back(arr[j++]);
+    }
+    //copy the merged elements back to the original array
+    for(int idx=si,k=0; idx<=ei; idx++){ //idx is the index for original array, k is the index for temp array
+        arr[idx] = temp[k++]; //copying back to original array
+    }
+}
+
+void mergeSort(int arr[], int si, int ei){
+    if(si>=ei){
+        return;
+    }
+    int mid=si+(ei-si)/2; //to avoid overflow
+    mergeSort(arr, si, mid);//left half
+    mergeSort(arr, mid+1, ei);//right half
+    //merge
+    merge(arr, si, mid, ei);
+}
+
+int partition(int arr[], int si, int ei){
+    int pivot = arr[ei]; //choosing the last element as pivot
+    int i=si-1; //pointer for smaller element
+    for(int j=si; j<ei; j++){
+        if(arr[j]<pivot){
+            i++;
+            swap(arr[i], arr[j]); //swap if element is smaller than pivot
+        }
+    }
+    i++;
+    swap(arr[i], arr[ei]); //place pivot in the correct position
+    return i; //return the index of pivot
+}
+
+void quickSort(int arr[], int si, int ei){ // time complexity: O(n log n) on average, O(n^2) in worst case (when the smallest or largest element is always chosen as pivot)
+    if(si>=ei){
+        return;
+    }
+    int pivotIdx = partition(arr, si, ei);
+    quickSort(arr, si, pivotIdx-1); //left half
+    quickSort(arr, pivotIdx+1, ei); //right half
+}
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int arr[7] = {6, 7, 3, 4, 1, 2, 5};
+    int n = 7;
+   // mergeSort(arr, 0, n-1);
+    quickSort(arr, 0, n-1);
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
